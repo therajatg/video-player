c1fdda08bff648d8323e3b8e34e27044e4332bed
therajatg <dguptarajat@gmail.com>
2024-06-10 18:24:53 +0530

    close contextMenu

--- src/components/VideoContextMenu.jsx
+++ src/components/VideoContextMenu.jsx
@@ -1,3 +1,4 @@
+import { useClickAway } from "@uidotdev/usehooks";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
@@ -10,31 +11,43 @@ export const VideoContextMenu = ({
   addComment,
 }) => {
   const { show, x, y } = contextMenu;
+
   const [showCommentBox, setShowCommentBox] = useState(false);
+  const ref = useClickAway(() => {
+    setContextMenu((prev) => {
+      return { ...prev, show: false };
+    });
+  });
 
   return (
-    <div className="absolute" style={{ top: `${y}px`, left: `${x}px` }}>
-      {showCommentBox ? (
-        <motion.div className="flex w-[300px] z-10" drag>
-          <textarea
-            type="text"
-            value={currentComment}
-            onChange={(e) => {
-              setCurrentComment(e.target.value);
-            }}
-            placeholder="Add a comment"
-            className={`w-full p-2 rounded-md ${selectedFont}`}
-          />
-          <button onClick={addComment}>Add</button>
-        </motion.div>
-      ) : (
-        <button
-          className="absolute z-10"
-          onClick={() => setShowCommentBox(true)}
-        >
-          Comment
-        </button>
-      )}
-    </div>
+    contextMenu.show && (
+      <div
+        className="absolute"
+        style={{ top: `${y}px`, left: `${x}px` }}
+        ref={ref}
+      >
+        {showCommentBox ? (
+          <motion.div className="flex w-[300px] z-10" drag>
+            <textarea
+              type="text"
+              value={currentComment}
+              onChange={(e) => {
+                setCurrentComment(e.target.value);
+              }}
+              placeholder="Add a comment"
+              className={`w-full p-2 rounded-md ${selectedFont}`}
+            />
+            <button onClick={addComment}>Add</button>
+          </motion.div>
+        ) : (
+          <button
+            className="absolute z-10"
+            onClick={() => setShowCommentBox(true)}
+          >
+            Comment
+          </button>
+        )}
+      </div>
+    )
   );
 };
